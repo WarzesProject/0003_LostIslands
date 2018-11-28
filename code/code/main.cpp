cd79f741c8661d3bc6269b797bc8845a88b9525f
warzes <warzes@mail.ru>
2018-11-28 15:03:17 +1000

    - проект с SDL

---
+++ code/code/main.cpp
@@ -0,0 +1,14 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include "stdafx.h"
+//-----------------------------------------------------------------------------
+#if COMPILER_MSVC
+#	pragma comment(lib, "lib/x86/SDL2.lib")
+#	pragma comment(lib, "lib/x86/SDL2main.lib")
+#endif
+//-----------------------------------------------------------------------------
+int main(int, char*[])
+{
+	return 0;
+}
+//-----------------------------------------------------------------------------
---
+++ code/code/stdafx.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "Base.h"
+
+#if COMPILER_MSVC
+#	pragma warning(push, 1)
+//#	pragma warning(disable: 0000)
+#endif
+
+#include <SDL2/SDL.h>
+
+#if COMPILER_MSVC
+#	pragma warning(pop)
+#endif
